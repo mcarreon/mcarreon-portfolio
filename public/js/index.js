5cb99ca9c77885f848ae7261ecf4d2f6d861f344
mcarreon <carryon2095@gmail.com>
2018-09-17 18:43:55 -0700

    updated index.js

--- public/js/index.js
+++ public/js/index.js
@@ -92,7 +92,3 @@ var handleDeleteBtnClick = function() {
     refreshExamples();
   });
 };
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
